d7ca57f21d2d983cb946284e0c7571163c8de268
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2025-01-27 09:40:38 -0300

    Alteração header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ import { Container } from "./Container";
 export function Header() {
     return (
         <header className="relative flex items-center w-full h-20 bg-primary-orange">
-            <div className="absolute top-0 right-0 bg-primary-blue w-[19%] h-full z-0"></div>
+            <div className="absolute top-0 right-0 bg-primary-blue w-[27%] h-full z-0"></div> {/* div que faz o btn azul de usuário ir até o final azul */}
             <Container>
                 <div className="flex flex-1 items-center justify-between">
                     <div className="flex items-center gap-14">
