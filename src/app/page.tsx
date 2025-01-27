7668c7fd290e1a7754773387485611595e6c718e
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2025-01-27 10:07:52 -0300

    Ajustando code para deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { SectionHero } from "@/components/SectionHero";
 import { SectionServices } from "@/components/SectionServices";
-import Image from "next/image";
 
 export default function Home() {
   return (
