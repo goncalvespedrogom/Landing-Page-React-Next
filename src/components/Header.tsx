a521119e2ba3f090e48369af34b7cf93e922b47e
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2025-01-29 12:44:22 -0300

    Alterando btn nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,52 +2,42 @@ import Image from "next/image";
 
 import { ItemMenu } from "./ItemMenu";
 
-import IconUser from '@/assets/icon-user-full.svg'
+import IconUser from "@/assets/icon-user-full.svg";
 import { Container } from "./Container";
-import LogoConnect from '@/assets/logo-connect-bank.png'
+import LogoConnect from "@/assets/logo-connect-bank.png";
 
 export function Header() {
-    return (
-        <header className="relative flex items-center h-20 w-full bg-primary-white">
-            <Container>
-                <Image 
-                    src={LogoConnect}
-                    alt="Logo"
-                    className="w-40"
-                />
-                <div className="flex flex-1 items-center justify-center">
-                        <ul className="flex items-center gap-12">
-                            <li>
-                                <ItemMenu 
-                                    name="Para você"
-                                />
-                            </li>
-                            <li>
-                                <ItemMenu 
-                                    name="Para empresas"
-                                />
-                            </li>
-                            <li>
-                                <ItemMenu 
-                                    name="Serviços"
-                                />
-                            </li>
-                            <li>
-                                <ItemMenu 
-                                    name="Ajuda"
-                                />
-                            </li>
-                        </ul>
-                    </div>
-                <button className="flex items-center gap-4 border-solid border-second-green border-[2px] px-4 py-3 rounded-2xl">
-                    <Image 
-                        src={IconUser}
-                        alt="Ícone do Usuário"
-                        className="w-7"
-                    />
-                    <span className="text-second-green font-bold">Acessar conta</span>
-                </button>
-            </Container>
-        </header>
-    )
+  return (
+    <header className="relative flex items-center h-24 w-full bg-primary-white">
+      <Container>
+        <Image src={LogoConnect} alt="Logo" className="w-40" />
+        <div className="flex flex-1 items-center justify-center">
+          <ul className="flex items-center gap-12">
+            <li>
+              <ItemMenu name="Para você" />
+            </li>
+            <li>
+              <ItemMenu name="Para empresas" />
+            </li>
+            <li>
+              <ItemMenu name="Serviços" />
+            </li>
+            <li>
+              <ItemMenu name="Ajuda" />
+            </li>
+          </ul>
+        </div>
+        <button
+          className="flex items-center gap-4 px-10 py-4 bg-second-green rounded-xl shadow-xl"
+          style={{
+            backgroundImage:
+              "linear-gradient(to right, #53d119, #42b60d, #309700)",
+          }}
+        >
+          <Image src={IconUser} alt="Ícone do Usuário" className="w-7" />
+          <span className="text-white font-bold">Acessar conta</span>
+        </button>
+      </Container>
+    </header>
+  );
 }
