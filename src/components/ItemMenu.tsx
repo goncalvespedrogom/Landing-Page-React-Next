5a4be986d617aa8c74ff50c263bd7286769c9a08
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2025-01-29 11:54:43 -0300

    Alteração txt nav

--- src/components/ItemMenu.tsx
+++ src/components/ItemMenu.tsx
@@ -9,7 +9,7 @@ type Props = {
 export function ItemMenu({ name } : Props) {
   return (
     <button className="flex items-center gap-3">
-      <span className="text-[#646464] font-medium">{name}</span>
+      <span className="text-[#646464] text-sm font-medium tracking-widest uppercase">{name}</span>
       <Image src={Arrow} alt="Seta para baixo" />
     </button>
   );
