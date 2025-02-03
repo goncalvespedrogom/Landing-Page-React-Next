ca55593c210724c4a64e0b8f0a9e348af8b13dcf
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2025-02-03 12:07:16 -0300

    Add responsivo lista items footer

--- src/components/ItemsFooter.tsx
+++ src/components/ItemsFooter.tsx
@@ -3,7 +3,7 @@ import ItemFooter from "@/components/ItemFooter";
 
 export function ItemsFooter() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-0 sm:mr-[-3rem] px-5 py-16 w-full mx-auto">
+    <div className="flex flex-col text-center sm:grid sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-0 sm:mr-[-3rem] px-5 pt-14 pb-8 sm:py-16 w-full mx-auto">
       <ItemFooter Links={PRODUTOS} title="PRODUTOS" />
       <ItemFooter Links={RECURSOS} title="RECURSOS" />
       <ItemFooter Links={EMPRESA} title="EMPRESA" />
